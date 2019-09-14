74caaa8f4ad2b27306197d6bee1fe8faf10fef0b
Kamil Owczarek <k.owczarek86@gmail.com>
2019-09-14 16:59:37 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kamil Owczarek on 14/09/2019.
+//  Copyright © 2019 Kamil Owczarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
